1e0a15bcbacf0474e47faf2964c5a4c6bbc37959
yang <94279769+nonetht@users.noreply.github.com>
2025-09-08 19:19:12 +0800

    day1's tasks finished

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -11,6 +11,8 @@ public interface EmployeeMapper {
      * 根据用户名查询员工
      * @param username
      * @return
+     *
+     * 简单sql使用注解方式来处理，较为复杂的比如动态sql则是用xml方式。
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
